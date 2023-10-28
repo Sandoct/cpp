9840140b3d06efb95e0a9f00ee0b41f373d56e73
Sandoct <gpouzet42@gmail.com>
2023-10-28 19:06:20 +0200

    ex01

--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -1,29 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gpouzet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/28 16:21:44 by gpouzet           #+#    #+#             */
-/*   Updated: 2023/10/28 16:22:06 by gpouzet          ###   ########.fr       */
+/*   Created: 2023/10/28 16:23:19 by gpouzet           #+#    #+#             */
+/*   Updated: 2023/10/28 19:06:01 by gpouzet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
 
-int	main (int argc, char **argv)
-{
-	int	i;
+class	Contact {
 
-	i = 0;	
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		while (i++ < argc && argv[i])
-			for (int j = 0; argv[i][j]; j++)
-				std::cout << (char) (std::toupper(argv[i][j]));
-		std::cout << std::endl;
-	}
+public :
+	char	*f_name;
+	char	*l_name;
+	char	*n_name;
+	int		*f_numb;
+	char	*d_secr;
+
+	Contact( void );
+	~Contact( void );
+}
+
+class	PhoneBook {
+
+public :
+	int[8]	contact;
+
+	public	ADD( Contact );
+	public	SEARCH( Contact );
+	public	EXIT( void );
+	PhoneBook( void );
+	~PhoneBook( void );
 }
