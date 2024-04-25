9a1c6c9464de0f32a821b5ca0ee70dac4d22506a
Sandoct <gpouzet42@gmail.com>
2024-04-25 15:49:13 +0200

    mod07 finished

--- Mod07/ex02/Array.hpp
+++ Mod07/ex02/Array.hpp
@@ -5,15 +5,79 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: r <marvin@42.fr>                           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/09 18:53:44 by r                 #+#    #+#             */
-/*   Updated: 2024/04/09 18:59:56 by r                ###   ########.fr       */
+/*   Created: 2024/04/10 14:37:09 by r                 #+#    #+#             */
+/*   Updated: 2024/04/25 15:23:57 by r                ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef Array_HPP
-# define Array_hpp
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
 # include <iostream>
 # include <string>
 
-template<class T>
+template<typename T>
+
+class Array
+{
+	private:
+		T		*_array;
+		size_t	_size;
+ 
+	public:
+		Array() : _array(new T[0]), _size(0) 
+		{
+			std::cout << "Array constructor called" << std::endl;
+		};
+		Array(size_t n) : _array(new T[n]), _size(n)
+		{
+			std::cout << "Array constructor called" << std::endl;
+			//for (unsigned int i = 0; i < n; i++)
+			//	this->_array[n] = 0;
+		};
+		Array( const Array &rhs)
+		{
+			std::cout << "Array copy constructor called" << std::endl;
+			*this = rhs;
+		};
+		~Array()
+		{
+			if (this->_array != NULL)
+				delete []	this->_array;
+			std::cout << "Array destructor called" << std::endl;
+		};
+		Array& operator=( const Array &rhs)
+		{
+			if (this->_array != NULL)
+				delete [] this->_array;
+			if (this->_size != rhs->_size)
+			{
+				this->_size = rhs->_size;
+				this->_array = new T[this->_size];
+			}
+			for (size_t i = 0; i < _size; i++)
+				_array[i] = rhs->_array[i];
+			return (*this);
+		};
+		T&	operator[](size_t index)
+		{
+			if (index >= this->_size)
+				throw Array::out_of_bound_exception();
+			return (this->_array[index]);
+		};
+		const	T&operator[](size_t index)const
+		{
+			if (index >= this->_size)
+				throw Array::out_of_bound_exception();
+			return (this->_array[index]);
+		};
+		class	out_of_bound_exception : public	std::exception
+		{
+			public:
+				char const	*what() const throw()
+				{
+					return "Index is out of bound.";
+				}
+		};
+}; 
+ 
 #endif
--- Mod07/ex02/main.cpp
+++ Mod07/ex02/main.cpp
@@ -6,8 +6,75 @@
 /*   By: r <marvin@42.fr>                           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 18:53:23 by r                 #+#    #+#             */
-/*   Updated: 2024/04/09 18:53:26 by r                ###   ########.fr       */
+/*   Updated: 2024/04/25 15:45:56 by r                ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Array.hpp"
 
+int	main()
+{
+	size_t	max = 6;
+	// Array of int//
+	Array<int>	decimal(max);
+	std::cout << std::endl;
+	std::cout << "Array content" << std::endl;
+	for (size_t i = 0; i < max; i++)
+	{
+		decimal[i] = i;
+		std::cout << decimal[i] << std::endl;
+	}
+	std::cout << std::endl;
+	// Array out of bound//
+	std::cout << "trying index + 1" << std::endl;
+	try
+	{
+		decimal[max + 1];
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << "trying negative index" << std::endl;
+	try
+	{
+		decimal[-2];
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	// Array of char //
+	std::cout << std::endl;
+	Array<char>	character(max);
+	std::cout << std::endl;
+	std::cout << "Array content" << std::endl;
+	for (size_t i = 0; i < max; i++)
+	{
+		character[i] = i + 97;
+		std::cout << character[i] << std::endl;
+	}
+	// Array out of bound//
+	std::cout << std::endl;
+	std::cout << "trying index + 1" << std::endl;
+	try
+	{
+		character[max + 1];
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << "trying negative index" << std::endl;
+	try
+	{
+		character[-2];
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+	return (0);
+}
